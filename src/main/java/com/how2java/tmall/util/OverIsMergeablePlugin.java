8859b43efd5e7ead91f4ed056f120b8bff4fa3ae
sunpeng <3147449490_njuswi@xly.bce.baidu.com>
2020-04-22 17:10:52 +0800

    test

---
+++ src/main/java/com/how2java/tmall/util/OverIsMergeablePlugin.java
@@ -0,0 +1,28 @@
+package com.how2java.tmall.util;
+
+import org.mybatis.generator.api.GeneratedXmlFile;
+import org.mybatis.generator.api.IntrospectedTable;
+import org.mybatis.generator.api.PluginAdapter;
+
+import java.lang.reflect.Field;
+import java.util.List;
+
+public class OverIsMergeablePlugin extends PluginAdapter {
+    @Override
+    public boolean validate(List<String> list) {
+        return true;
+    }
+
+    @Override
+    public boolean sqlMapGenerated(GeneratedXmlFile sqlMap, IntrospectedTable introspectedTable) {
+        try{
+            Field field = sqlMap.getClass().getDeclaredField("isMergeable");
+            //当isAccessible()的结果是false时不允许通过反射访问该字段
+            field.setAccessible(true);
+            field.setBoolean(sqlMap, false);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return true;
+    }
+}
